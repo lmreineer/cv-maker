3c40c728dc14cfea164c3b7fe24254ae53cae223
Lance <lmreineer@gmail.com>
2023-08-03 17:57:14 +0800

    Make job responsibilities input form auto-growing by adding a library

--- src/components/form/WorkResponsibilitiesForm.js
+++ src/components/form/WorkResponsibilitiesForm.js
@@ -1,23 +1,38 @@
+import React, { useRef, useEffect } from 'react';
+import autosize from 'autosize';
+
 import { useNavigate } from 'react-router-dom';
 
 const WorkResponsibilitiesForm = () => {
+    const textareaRef1 = useRef(null);
+    const textareaRef2 = useRef(null);
+
+    useEffect(() => {
+        autosize(textareaRef1.current);
+        autosize(textareaRef2.current);
+    });
+
     const navigate = useNavigate();
     const navigateBack = () => navigate('/work-history');
 
     return (
         <form className="flex w-1/2 flex-col justify-center">
             <h1 className="mb-10 mt-16 text-center font-cabin text-4xl font-semibold tracking-wider text-very-dark-yellow-green">
-                Work Responsibilities:
+                Describe your job responsibilities here:
             </h1>
             <div className="flex flex-col justify-center">
                 <textarea
                     type="text"
-                    className="h-28 resize-none break-all border-2 border-b-2 p-4 outline-0"
+                    placeholder="Job Responsibilities"
+                    className="h-[3.6rem] resize-none break-all border-2 border-b-2 p-4 outline-0"
+                    ref={textareaRef1}
                     maxLength={250}
                 />
                 <textarea
                     type="text"
-                    className="h-28 resize-none break-all border-2 border-t-0 p-4 outline-0"
+                    placeholder="Job Responsibilities"
+                    className="h-[3.6rem] resize-none break-all border-2 border-t-0 p-4 outline-0"
+                    ref={textareaRef2}
                     maxLength={250}
                 />
             </div>
