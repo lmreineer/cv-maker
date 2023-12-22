3565c7a037e23347cf13fdc1c7606b4270ece2cd
lance <lmreineer@gmail.com>
2023-12-22 14:47:34 +0800

    Adjust styles

--- src/components/path/Final.js
+++ src/components/path/Final.js
@@ -4,7 +4,7 @@ const Final = ({ componentRef }) => {
     return (
         <CVModal
             componentRef={componentRef}
-            modalSizing="border-2 m-10 w-[36rem] p-10 h-screen md:min-h-[43.4rem] md:tall:min-h-screen"
+            modalSizing="border-2 m-10 w-[36rem] p-10 md:min-h-[43.4rem] md:tall:min-h-screen"
         />
     );
 };
